f5f89a84db97f9e7ea912f99dda8667d8a30f29c
Pedro Gomes <up202108825@up.pt>
2023-07-14 15:25:57 +0100

    search still pending

--- week1/encyclopedia/views.py
+++ week1/encyclopedia/views.py
@@ -8,3 +8,25 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def wiki(request, title):
+    if util.get_entry(title) is None:
+        return render(request, "encyclopedia/error.html",{
+                      "title" : title})
+    return render(request, "encyclopedia/wiki.html", {
+        "title": title.capitalize(),
+        "content": util.get_entry(title)
+    })
+
+def search(request, query):
+    result = util.get_entry(query)
+    if result is None:
+        results = [entry for entry in util.list_entries() if query in entry]
+        return render(request, "encyclopedia/search.html",{
+            "query": query,
+            "results": results
+        })
+    return render(request, "encyclopedia/wiki.html",{
+        "title": query.capitalize(),
+        "content": util.get_entry(query)
+    })
+
