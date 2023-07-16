d747e36aed1e4619d53bf9d75d119bc3b13aade6
Pedro Gomes <up202108825@up.pt>
2023-07-16 20:16:02 +0100

    models added

--- project2/auctions/models.py
+++ project2/auctions/models.py
@@ -4,3 +4,31 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+class Listing(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=20)
+    description = models.CharField(max_length=200)
+    image = models.URLField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    starting_bid = models.IntegerField()
+    current_bid = models.IntegerField(default=starting_bid)
+    starttime = models.DateTimeField(auto_now_add=True)
+    endtime = models.DateTimeField()
+
+class Bid(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    datetime = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete= models.CASCADE)
+    content = models.CharField(max_length=200)
+    datetime = models.DateTimeField(auto_now_add=True)
+    
+    
